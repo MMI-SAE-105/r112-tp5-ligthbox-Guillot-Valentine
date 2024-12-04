69b59cea1379c2b8c3752c47b74069f638e11440
Guillot-Valentine <valentine.guillot@edu.univ-fcomte.fr>
2024-12-04 17:28:47 +0100

    debut Java script, debut css

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,13 @@
 // *** LIGHTBOX ***
-const lightBox = document.querySelector("#lightbox"); 
+const lightBox = document.querySelector("#lightbox");
+const lightBoxImage = lightBox.querySelector("img");
 
-document.querySelectorAll("img").forEach(img => { 
-    img.addEventListener("click", () => {  
-        lightBox.showModal(); 
-    }); 
-}); 
+document.querySelectorAll("img").forEach(img => {
+    img.addEventListener("click", () => {
+      lightBox.showModal();
+    });
+  });
 
-lightBox.addEventListener("click", (evt) => { 
-    if (evt.target === lightBox) lightBox.close(); 
-});
+  lightBox.addEventListener("click", (evt) => {
+    if (evt.target === lightBox) lightBox.close();
+  });
