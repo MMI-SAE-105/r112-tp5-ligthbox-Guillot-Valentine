8ca3db52a5b22a1afedfe0aefc72d4935a54f7b3
Guillot-Valentine <valentine.guillot@edu.univ-fcomte.fr>
2024-12-04 17:16:39 +0100

    Une partie de js et début CSS

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
 // *** LIGHTBOX ***
+const lightBox = document.querySelector("#lightbox"); 
+
+document.querySelectorAll("img").forEach(img => { 
+    img.addEventListener("click", () => {  
+        lightBox.showModal(); 
+    }); 
+}); 
+
+lightBox.addEventListener("click", (evt) => { 
+    if (evt.target === lightBox) lightBox.close(); 
+});
